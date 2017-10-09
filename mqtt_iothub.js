8bc04b67d01b744c8edbf0766cd19f5c5da5757a
Brooks Wood <bwood@forwardthinking.us>
2017-10-08 21:42:41 -0600

    Initial Commit

---
+++ mqtt_iothub.js
@@ -0,0 +1,39 @@
+'use strict';
+//MQTT_TCP
+var mqtt = require('mqtt');
+var mqttServer = 'mqtt://localhost:1883'
+var options = {
+ username: 'locateplus',
+ password: '922a7a2e56',
+};
+
+var mqttclient = mqtt.connect(mqttServer, options); //var client = mqtt.connect(mqttServer, options);
+
+//IoT HUB
+var clientFromConnectionString = require('azure-iot-device-mqtt').clientFromConnectionString;
+var Message = require('azure-iot-device').Message;
+
+var connectionString = 'HostName=LCPTEST.azure-devices.net;DeviceId=lcp-test;SharedAccessKey=Byy2efMveKTCGNOVBssjkgJH4uFX0KBfNYgazg23Oyg=';
+var iotclient = clientFromConnectionString(connectionString);
+
+mqttclient.subscribe('GW/Event');
+
+mqttclient.on('message', function (topic, message) {
+sendMessage(message.toString());
+});
+
+function sendMessage(payload) {
+  var message = new Message(payload);
+  console.log("Sending message: " + message.getData());
+  iotclient.sendEvent(message);
+};
+
+
+// function sendMessage(payload) {
+//   // var rtls = JSON.parse(payload);
+//   // var rtls = payload;
+//   // var rtlspayload = JSON.stringify({deviceId:'lcp-test', EventTime:rtls.EventTime, TimeStamp:rtls.TimeStamp, GatewayID:rtls.GatewayID, TagID:rtls.TagID, rssi:rtls.rssi, Transaction:rtls.Transaction, Temperature:rtls.Temperature, Humidity:rtls.Humidity, SensorID:rtls.SensorID, DispenserStatus:rtls.DispenserStatus, RefillSerial:rtls.RefillSerial, SensorBattery:rtls.SensorBattery, TagBattery:rtls.TagBattery });
+//   var message = new Message(payload);
+//   console.log("Sending message: " + message.getData());
+//   iotclient.sendEvent(message);
+// };
